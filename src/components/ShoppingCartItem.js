e4ac629ee10c67006b26aacfb71ca3f58b26e595
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-10 13:06:04 -0700

    shoppingcartitem.js: reformat to also use context, add in qty, remove item

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {CartContext} from '../Contexts/CartContext';
 
 const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
-
-
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+    const {image, title, price, quantity} = props.item;
+    const {removeItem, changeQty} = useContext(CartContext);
+    return (
+        <div className="shopping-cart_item">
+            <img src={image} alt={`${title} book`}/>
+            <div>
+                <h1>{title}</h1>
+                <p>$ {price}</p>
+                <label>Qty: </label>
+                <input
+                    type='number'
+                    onChange={e => changeQty(title, e.target.value)}
+                    value={quantity}
+                    min='1'
+                    style={{width: 35}}
+                />
+                <button
+                    onClick={e => removeItem(e, props.index, props.item)}
+                >
+                    Remove from cart
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Item;
